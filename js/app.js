198ed395da20cab497067d37bf7dca9fd4261eaa
eileen <zwleileen@gmail.com>
2024-12-17 19:02:38 +0800

    feat:completed assignment

--- js/app.js
+++ js/app.js
@@ -3,33 +3,35 @@ let board;
 let turn;
 let winner;
 let tie;
+let gameStatus;
 
 //step 2: store cached element references
 const squareEls = document.querySelectorAll(".sqr");
 const messageEl = document.getElementById("message");
 // console.log((messageEl.textContent = "hello"));
 const player = {
-  1: "X",
+  1: "X", //1 is a number, X is a string
   "-1": "0",
 };
+// console.log(player[-1]); returns 0
+// console.log(typeof player[1]); returns string
 
 //step 3: initialise game state and call function
 const init = () => {
-  board = ["X", "X", "X", "", "", "", "", "", ""];
-  turn = "1";
+  //similarly, this defines what init will do when called
+  board = ["", "", "", "", "", "", "", "", ""];
+  turn = 1; //set number 1, not string "1"
   winner = false;
   tie = false;
-  render();
+  render(); //here's where render actually gets called after we call init at the next line
 };
 
+// console.log(typeof turn); returns number
+// console.log(player[turn]); returns X because turn is initialised as 1
+// console.log(gameStatus);
+
 //step 4: update board and message
-gameStatus = `${player[turn]}'s Turn`;
-const render = () => {
-  board.forEach((squareEl, idx) => {
-    squareEls[idx].textContent = squareEl;
-  });
-  messageEl.textContent = gameStatus;
-};
+
 //step 5: define winningCombos
 const checkWinner = () => {
   const winningCombos = [
@@ -52,7 +54,6 @@ const checkWinner = () => {
     if (a && a === b && a === c) {
       //checks that the entire row of winningCombo[i] is the same
       winner = a; //
-      gameStatus = `${winner} Wins`;
       return; //as soon as we find a winner, we exit function, instead of continuing to check all other combinations
     }
   }
@@ -67,8 +68,26 @@ const handleClick = (event) => {
     board[idx] = player[turn];
   }
   checkWinner(); //checks if this move creates a winner
-  turn *= -1; //multiplies turn by -1 to switch players i.e. 1 becomes -1, -1 becomes 1
-  render();
+  if (winner) {
+    gameStatus = `${winner} Wins`;
+  } else {
+    turn *= -1; //multiplies turn by -1 to switch players i.e. 1 becomes -1, -1 becomes 1
+    gameStatus = `${player[turn]}'s Turn`;
+  }
+  render(); //update the display
 };
 
-init();
+const render = () => {
+  //defines what render will do when called, but it is NOT called yet
+  //declare render first before running it below
+  board.forEach((squareEl, idx) => {
+    squareEls[idx].textContent = squareEl;
+  });
+  messageEl.textContent = gameStatus;
+};
+
+squareEls.forEach((square) => {
+  square.addEventListener("click", handleClick);
+});
+
+init(); //init is called here
