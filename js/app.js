6ce346dec70d72bb0d21f6ce81017d78f13652a2
eileen <zwleileen@gmail.com>
2024-12-18 00:42:14 +0800

    feat: added reset and tie functions

--- js/app.js
+++ js/app.js
@@ -1,40 +1,39 @@
-//step 1: define required variables
 let board;
 let turn;
 let winner;
 let tie;
 let gameStatus;
 
-//step 2: store cached element references
 const squareEls = document.querySelectorAll(".sqr");
 const messageEl = document.getElementById("message");
-// console.log((messageEl.textContent = "hello"));
 const player = {
-  1: "X", //1 is a number, X is a string
+  1: "X", //notes: 1 is a number, X is a string
   "-1": "0",
 };
-// console.log(player[-1]); returns 0
-// console.log(typeof player[1]); returns string
+// test: console.log(player[-1]); returns 0
+// test: console.log(typeof player[1]); returns string
 
-//step 3: initialise game state and call function
+//notes: add reset button
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+resetButton.style.marginTop = "20px";
+const boardElement = document.querySelector(".board");
+boardElement.appendChild(resetButton);
+
+//notes: initialise variables, when init is called at the end, init() will run, incl the render()
 const init = () => {
-  //similarly, this defines what init will do when called
   board = ["", "", "", "", "", "", "", "", ""];
   turn = 1; //set number 1, not string "1"
   winner = false;
   tie = false;
-  render(); //here's where render actually gets called after we call init at the next line
+  gameStatus = "";
+  render(); //notes: render() runs when init is called at the end
 };
 
-// console.log(typeof turn); returns number
-// console.log(player[turn]); returns X because turn is initialised as 1
-// console.log(gameStatus);
-
-//step 4: update board and message
-
-//step 5: define winningCombos
+//notes: define winningCombos and call a winner
 const checkWinner = () => {
   const winningCombos = [
+    //notes: combinations that when filled with same X or 0 would make winningCombo
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -44,50 +43,60 @@ const checkWinner = () => {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  //check if any winningCombo is achieved
+
   for (let i = 0; i < winningCombos.length; i++) {
-    let combo = winningCombos[i]; //loops through all winningCombos
-    let a = board[combo[0]]; //loops within entire row of winningCombo[i]
+    let combo = winningCombos[i]; //notes: loops through all winningCombos
+    let a = board[combo[0]]; //notes: loops within entire row of winningCombo[i]
     let b = board[combo[1]];
     let c = board[combo[2]];
 
     if (a && a === b && a === c) {
-      //checks that the entire row of winningCombo[i] is the same
-      winner = a; //
-      return; //as soon as we find a winner, we exit function, instead of continuing to check all other combinations
+      //notes: check that the entire row of winningCombo[i] is the same
+      winner = a;
+      return; //notes: as soon as we find a winner, we exit function, instead of continuing to check all other combinations
     }
+  }
+  if (!winner && !board.includes("")) {
+    //notes: ! means not, so !winner means no winner, and since winner is false by default, !winner returns true
+    tie = true;
   }
 };
 
 const handleClick = (event) => {
-  const idx = event.target.id; //assigns id of the target HTML element when an event happened
+  const idx = event.target.id; //notes: assigns id (0 to 8) of the target HTML square element to idx when square is clicked
   if (board[idx] || winner) {
-    return;
+    return; //notes: exit the function if the board array has a value or there is a winner
   } else {
-    //exit function if board's square is filled or there is a winner
-    board[idx] = player[turn];
+    board[idx] = player[turn]; //notes: mark X or 0 in the board array initiated above
   }
-  checkWinner(); //checks if this move creates a winner
+  checkWinner(); //notes: checks if clicking the square creates a winner by running the checkWinner()
   if (winner) {
+    //notes: update gameStatus if there is winner
     gameStatus = `${winner} Wins`;
+  } else if (tie) {
+    gameStatus = `It's a Tie!`;
   } else {
-    turn *= -1; //multiplies turn by -1 to switch players i.e. 1 becomes -1, -1 becomes 1
+    turn *= -1; //notes: multiplies turn by -1 to switch players i.e. 1 becomes -1, -1 becomes 1
     gameStatus = `${player[turn]}'s Turn`;
   }
-  render(); //update the display
+  render(); //notes: runs render()
+};
+
+const handleReset = (event) => {
+  init();
 };
 
 const render = () => {
-  //defines what render will do when called, but it is NOT called yet
-  //declare render first before running it below
+  //notes: updates the visual display on HTML, defines what render will do when called, but it is NOT called yet
   board.forEach((squareEl, idx) => {
-    squareEls[idx].textContent = squareEl;
+    squareEls[idx].textContent = squareEl; //notes: squareEl is the value in the board array, and we assign it to the square elements in HTML
   });
   messageEl.textContent = gameStatus;
 };
 
-squareEls.forEach((square) => {
-  square.addEventListener("click", handleClick);
+squareEls.forEach((squareEl) => {
+  squareEl.addEventListener("click", handleClick);
 });
+resetButton.addEventListener("click", handleReset);
 
 init(); //init is called here
